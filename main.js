0630c3f065b0031583150144186e42d238f039ff
Thiago Diógenes Maia Gonçalves <thiago.maia@dotzmkt.com.br>
2022-03-30 14:34:11 -0300

    ajuste forms

--- main.js
+++ main.js
@@ -1,46 +1,54 @@
-let email = document.getElementById('email');
-let password = document.getElementById('password');
-let form = document.querySelector('form');
-let textEmail = document.getElementById('textEmail');
-let textPassword = document.getElementById('textPassword');
-let textForm = document.getElementById('textForm')
+let email = document.getElementById("email");
+let password = document.getElementById("password");
+let form = document.querySelector("form");
+let textForm = document.getElementById("textForm");
+let textEmail = document.getElementById("textEmail");
+let textPassword = document.getElementById("textPassword");
 
-
-form.addEventListener('submit', (event) => {
-
-  if(email.value == '' && password.value == '') {
-    textForm.textContent = 'Você precisa preencher todos os campos!'
-  } else if (validatorEmail(email.value) !== true) {
-    textEmail.textContent = "O campo e-mail deve estar no formato Ex: email@abc.com"
-  } else if(passwordValidator(password.value) !== true) {
-    textPassword.textContent = "A senha dever no minimo 6 caracteres, ao menos 1 numero e ao menos um caracter especial"
-  } else {
+form.addEventListener("submit", (e) => {
+  if (email.value == "" && password.value == "") {
+    textForm.textContent = "Você precisa preencher todos os campos!";
+  } else if (
+    validatorEmail(email.value) === true &&
+    validatorPassword(password.value) === true
+  ) {
     console.log(email.value);
     console.log(password.value);
-    textEmail.textContent = '';
-    textPassword.textContent = '';
-    textForm.textContent = ''
-    console.log('Você você será redirecionado para a página X');
-    clear()
+    textForm.textContent = "";
+    textEmail.textContent = "";
+    textPassword.textContent = "";
+  } else {
+    console.log("Requisição não atendida");
   }
-  
-  
-  event.preventDefault()
-})
 
+  e.preventDefault();
+});
+
+email.addEventListener("keyup", () => {
+  if (validatorEmail(email.value) !== true) {
+    textEmail.textContent = "O formato do email deve ser Ex: name@abc.com";
+  } else {
+    textEmail.textContent = "";
+  }
+});
+
+password.addEventListener("keyup", () => {
+  if (validatorPassword(password.value) !== true) {
+    textPassword.textContent =
+      "O formato da senha deve ser no min 6 caracteres";
+  } else {
+    textPassword.textContent = "";
+  }
+});
 
 function validatorEmail(email) {
-  let emailPattern = /^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})$/;
-  return emailPattern.test(email)
+  let emailPattern =
+    /^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})$/;
+  return emailPattern.test(email);
 }
 
-function passwordValidator(password) {
-  let passwordPattern = /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{6,16}$/;
-  return passwordPattern.test(password)
-}
-
-function clear() {
-  email.value = '';
-  password.value = ''
-
+function validatorPassword(password) {
+  let passwordPattern =
+    /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{6,16}$/;
+  return passwordPattern.test(password);
 }
